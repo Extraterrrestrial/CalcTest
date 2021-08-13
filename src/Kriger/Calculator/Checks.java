e05082fc0d77c6637f8c5c0cc84d252d0dfbd7fc
Yuri Kriger <yuri.kriger@gmail.com>
2021-08-13 21:47:02 +0600

    Remove one class

--- src/Kriger/Calculator/Checks.java
+++ src/Kriger/Calculator/Checks.java
@@ -2,20 +2,20 @@ package Kriger.Calculator;
 
 public class Checks {
 
-    public static boolean exist_rome(String arrstr) {
+    public static boolean exist_rome(String arr_str) {
         String[] massStringA = {"I","II","III","IV", "V", "VI", "VII", "VIII", "IX", "X"};
         for (String symbol_in_arr : massStringA) {
-            if (arrstr.equals(symbol_in_arr)) {
+            if (arr_str.equals(symbol_in_arr)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean exist_arabic(String arrstr) {
+    public static boolean exist_arabic(String arr_str) {
         String[] massStringA = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         for (String symbol_in_arr : massStringA) {
-            if (arrstr.equals(symbol_in_arr)) {
+            if (arr_str.equals(symbol_in_arr)) {
                 return true;
             }
         }
