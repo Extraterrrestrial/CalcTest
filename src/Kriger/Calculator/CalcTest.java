69847d49c528b30347a727abd8d6a4ece3ddcd16
Yuri Kriger <yuri.kriger@gmail.com>
2021-08-13 23:54:59 +0600

    changes in the method call

--- src/Kriger/Calculator/CalcTest.java
+++ src/Kriger/Calculator/CalcTest.java
@@ -6,9 +6,6 @@ class CalcTest {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Calculate calculate = new Calculate();
-        Checks check = new Checks();
-        Convert convert = new Convert();
 
         System.out.println("Введите, что нужно посчитать в виде 5 + 5 или V + V\nВводить нужно числа от 1 до 10\nДля завершения работы калбкулятора введите x:");
 
@@ -28,10 +25,10 @@ class CalcTest {
             String operate = input_line[1];
             String str2 = input_line[2];
 
-            if (check.exist_arabic(str1) && check.exist_arabic(str2) && operate.length() == 1) {
-                System.out.println("Ответ= " + calculate.calculate(Integer.valueOf(str1), Integer.valueOf(str2), operate.charAt(0)));
-            } else if (check.exist_rome(str1) && check.exist_rome(str2) && operate.length() == 1) {
-                System.out.println("Ответ= " + convert.arabic_to_rome(calculate.calculate(convert.rome_to_arabic(str1), convert.rome_to_arabic(str2), operate.charAt(0))));
+            if (Checks.exist_arabic(str1) && Checks.exist_arabic(str2) && operate.length() == 1) {
+                System.out.println("Ответ= " + Calculate.calculate(Integer.parseInt(str1), Integer.parseInt(str2), operate.charAt(0)));
+            } else if (Checks.exist_rome(str1) && Checks.exist_rome(str2) && operate.length() == 1) {
+                System.out.println("Ответ= " + Convert.arabic_to_rome(Calculate.calculate(Convert.rome_to_arabic(str1), Convert.rome_to_arabic(str2), operate.charAt(0))));
             } else {
                 System.out.println("Ошибка!");
                 System.exit(1);
